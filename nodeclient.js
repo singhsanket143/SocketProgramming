0a02b7a4e23e4d1044200fb14b2bd0ffedb946a6
Sanket Singh <singhsanket143@gmail.com>
2023-12-02 10:50:30 +0530

    Update nodeclient.js

--- nodeclient.js
+++ nodeclient.js
@@ -1,6 +1,6 @@
 const net = require('net');
 
-const client = net.createConnection({port: 8083}, () => {
+const client = net.createConnection({port: 8083}, serverStarted() => {
     console.log('Client connected');
     client.write("Hello from node client");
 })
